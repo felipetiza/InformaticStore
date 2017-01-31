82025149a6989aac68b61ad7b08741fddc925320
felipetiza <felipetiza5@gmail.com>
2017-01-31 23:45:11 +0100

    Menu screen now has responsive design

--- menu.php
+++ menu.php
@@ -20,15 +20,36 @@
 
 			// Get username from database
             $getusername = "SELECT name
-                         FROM customer
-                         WHERE idcustomer = {$_SESSION['iduser']}
-                        ";
+                            FROM customer
+                            WHERE idcustomer = {$_SESSION['iduser']};
+                           ";
 
             if ($result = $connection->query($getusername)) {
                 if ($result->num_rows > 0)
                     $username = $result->fetch_object()->name;
                 else
                     echo "Impossible to get the username";
+            }else
+                echo "Wrong Query";
+
+            // Get products data
+			$productName  = [];
+			$productPrice = [];
+			$productImage = [];
+
+            $getproduct = "SELECT *
+                           FROM product;
+                          ";
+
+            if ($result = $connection->query($getproduct)) {
+                if ($result->num_rows > 0){
+                	while($product = $result->fetch_object()){
+                		array_push($productName, $product->name);
+                		array_push($productPrice, $product->price);
+                		array_push($productImage, $product->urlimage);
+                	}
+                }else
+                    echo "Impossible to get the products";
             }else
                 echo "Wrong Query";
 		}else
@@ -44,13 +65,59 @@
         <?php echo "<p>Welcome, $username</p>" ?>
         <br>
 		<div id="content">
-
+			<div class="product">
+				<img src="resources/img/product/mouse.jpg">
+				<h3>Mouse</h3>
+				<p>14€</p>
+			</div>
+			<div class="product">
+				<img src="resources/img/product/mouse.jpg">
+				<h3>Mouse</h3>
+				<p>14€</p>
+			</div>
+			<div class="product">
+				<img src="resources/img/product/mouse.jpg">
+				<h3>Mouse</h3>
+				<p>14€</p>
+			</div>
+			<div class="product">
+				<img src="resources/img/product/mouse.jpg">
+				<h3>Mouse</h3>
+				<p>14€</p>
+			</div>
+			<div class="product">
+				<img src="resources/img/product/mouse.jpg">
+				<h3>Mouse</h3>
+				<p>14€</p>
+			</div>
+			<div class="product">
+				<img src="resources/img/product/mouse.jpg">
+				<h3>Mouse</h3>
+				<p>14€</p>
+			</div>
+			<div class="product">
+				<img src="resources/img/product/mouse.jpg">
+				<h3>Mouse</h3>
+				<p>14€</p>
+			</div>
+			<div class="product">
+				<img src="resources/img/product/mouse.jpg">
+				<h3>Mouse</h3>
+				<p>14€</p>
+			</div>
+			<div class="product">
+				<img src="resources/img/product/mouse.jpg">
+				<h3>Mouse</h3>
+				<p>14€</p>
+			</div>
 		</div>
 	</div>
 
 
 
-
+<!-- 	foreach($productName as $p)
+    		echo $p;
+-->
 
 
 
